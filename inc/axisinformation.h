27240abf99e0428c3910c69804a7142c16944f4b
Michael Reuter <mareuternh@gmail.com>
2011-04-14 15:17:40 -0400

    Filling out axis information class.

--- inc/axisinformation.h
+++ inc/axisinformation.h
@@ -8,10 +8,25 @@
 #ifndef AXISINFORMATION_H_
 #define AXISINFORMATION_H_
 
+#include <string>
+
 class AxisInformation {
 public:
 	AxisInformation();
 	virtual ~AxisInformation() {}
+
+	double getMaximum() { return this->maximum; }
+	double getMinimum() { return this->minimum; }
+	std::string getTitle() { return this->title; }
+
+	void setMaximum(double max) { this->maximum = max; }
+	void setMinimum(double min) { this->minimum = min; }
+	void setTitle(std::string title) { this->title = title; }
+
+private:
+	std::string title;
+	double minimum;
+	double maximum;
 };
 
 #endif /* AXISINFORMATION_H_ */
--- src/axisinformation.cpp
+++ src/axisinformation.cpp
@@ -9,5 +9,7 @@
 
 AxisInformation::AxisInformation()
 {
-
+	this->title = std::string("");
+	this->minimum = -9999999.0;
+	this->maximum = -9999999.0;
 }

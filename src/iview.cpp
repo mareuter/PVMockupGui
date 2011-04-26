ed3780863c14302d4c43bf22599bb5d0805152dd
Michael Reuter <mareuternh@gmail.com>
2011-04-26 11:33:58 -0400

    Indentation fix.

--- src/iview.cpp
+++ src/iview.cpp
@@ -17,19 +17,19 @@ IView::IView(QWidget *parent) : QWidget(parent)
 
 pqRenderView* IView::createRenderView(QWidget* widget)
 {
-  QHBoxLayout *hbox = new QHBoxLayout(widget);
-  hbox->setMargin(0);
+	QHBoxLayout *hbox = new QHBoxLayout(widget);
+	hbox->setMargin(0);
 
-  // Create a new render view.
-  pqObjectBuilder* builder = pqApplicationCore::instance()->getObjectBuilder();
-  pqRenderView *view = qobject_cast<pqRenderView*>(
-    builder->createView(pqRenderView::renderViewType(),
-      pqActiveObjects::instance().activeServer()));
-  //pqActiveObjects::instance().setActiveView(view);
+	// Create a new render view.
+	pqObjectBuilder* builder = pqApplicationCore::instance()->getObjectBuilder();
+	pqRenderView *view = qobject_cast<pqRenderView*>(
+			builder->createView(pqRenderView::renderViewType(),
+					pqActiveObjects::instance().activeServer()));
+	//pqActiveObjects::instance().setActiveView(view);
 
-  // Place the widget for the render view in the frame provided.
-  hbox->addWidget(view->getWidget());
-  return view;
+	// Place the widget for the render view in the frame provided.
+	hbox->addWidget(view->getWidget());
+	return view;
 }
 
 void IView::destroyFilter(pqObjectBuilder *builder, const QString &name)
